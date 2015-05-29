5b0e77f4e76cf18225338c5cf6123af67618c1b5
Paul Ogawa <paul.ogawa7@gmail.com>
2015-05-29 13:10:52 -0700

    Add jquerey functionality and text formatting

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,28 @@ var findAndReplace = function(sentence, word, replacementWord) {
   var clean = sentence.replace(/[|&;$%@"<>()+,]/g, "");
 
   var result = clean.replace(word, replacementWord);
+
   return result
 }
+
+$(document).ready(function(){
+
+  $("#name-changer").submit(function(event){
+
+    var sentence = ($("input#input-sentence").val());
+
+    var word = ($("input#input-word").val());
+
+    var replacementWord = ($("input#input-replacementWord").val());
+
+    var result = findAndReplace(sentence, word, replacementWord);
+
+    $(".output").text(result);
+
+    $("#result").show();
+
+    event.preventDefault();
+
+  });
+
+});
