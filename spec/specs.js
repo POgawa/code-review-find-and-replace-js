f2112743355a79cfe0a5ebaecf52b1c1cf0bcf60
Paul Ogawa <paul.ogawa7@gmail.com>
2015-05-29 11:38:39 -0700

    Add initial test

---
+++ spec/specs.js
@@ -0,0 +1,7 @@
+describe('findAndReplace', function() {
+
+  it("Finds a word in a string and replaces it with another word.", function() {
+    expect(findAndReplace("This is a string", "string", "test")).to.equal("This is a test");
+  });
+
+});
